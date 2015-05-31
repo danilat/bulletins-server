5803060181e9912d700a7e503b6ea5f5a1c13272
Dani Latorre <dani@danilat.com>
2015-06-01 01:15:29 +0200

    implemented create a bulletin

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Bulletin = require('./bulletin_model.js')
 var app = express();
 
 app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function (req, res) {
-  res.render('index');
+  Bulletin.find({}, function (err, docs) {
+    res.render('index', { bulletins: docs });
+  });
 });
 app.get('/bulletins/new', function (req, res) {
   res.render('new');
 });
+app.post('/bulletins', function (req, res) {
+  bulletin = new Bulletin({name: req.body.name, 
+        body: req.body.body});
+  bulletin.save(function(err){
+    res.redirect('/');
+  });
+});
+
 app.get('/api/bulletins', function (req, res) {
   Bulletin.find({}, function (err, docs) {
     res.jsonp(docs);
@@ -17,7 +30,11 @@ app.get('/api/bulletins', function (req, res) {
 });
 
 var server = app.listen(3000, function () {
-  mongoose.connect('mongodb://localhost/test');
+  if(process.env.NODE_ENV == 'test'){
+    mongoose.connect('mongodb://localhost/test');
+  }else{
+    mongoose.connect('mongodb://localhost/bulletins-server');
+  }
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
