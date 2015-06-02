df636062de9c3c88c179e36197b79759d8508b41
Dani Latorre <dani@danilat.com>
2015-06-02 03:40:25 +0200

    trying to fix mongo connection to mongolab

--- app.js
+++ app.js
@@ -60,10 +60,12 @@ app.get('/api/bulletins', function (req, res) {
 var server = app.listen(3000, function () {
   console.log(process.env.NODE_ENV)
   if(process.env.NODE_ENV == 'test'){
-
     mongoose.connect('mongodb://localhost/test');
   }else{
-    mongoose.connect('mongodb://localhost/bulletins-server');
+    var mongo_uri = process.env.MONGOLAB_URI || 
+      process.env.MONGOHQ_URL ||
+      'mongodb://localhost/bulletins-server'
+    mongoose.connect(mongo_uri);
   }
   var host = server.address().address;
   var port = server.address().port;
